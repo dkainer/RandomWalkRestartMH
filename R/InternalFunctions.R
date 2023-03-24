6f593cbfbc41305b8b8413fe99c62d4db52bed69
Matt Lane <lane.matthew.joseph@gmail.com>
2023-03-23 20:11:12 -0400

    updated to no longer throw warnings

--- R/InternalFunctions.R
+++ R/InternalFunctions.R
@@ -45,9 +45,9 @@ simplify.layers <- function(Input_Layer){
     
     ## Simple Graphs
     Layer <- 
-        igraph::simplify(Layer,remove.multiple = TRUE,remove.loops = TRUE, 
-            edge.attr.comb=mean)
-    
+        igraph::simplify(g1,remove.multiple = TRUE,remove.loops = TRUE, 
+            edge.attr.comb=list(weight=mean, type="first"))
+
     return(Layer)
 }
 
